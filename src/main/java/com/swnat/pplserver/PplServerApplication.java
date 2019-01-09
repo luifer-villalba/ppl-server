92f0a4c9b30d59eef3d6642ad47cfa0432d49853
Lufer Villalba <villalba@elumen.info>
2019-01-09 14:10:51 -0300

    Commit inicial a master.

---
+++ src/main/java/com/swnat/pplserver/PplServerApplication.java
@@ -0,0 +1,14 @@
+package com.swnat.pplserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PplServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PplServerApplication.class, args);
+    }
+
+}
+
